52242a43fff71fff3d4e3b4eb6b3a121f069e0b5
shanME <giorgie103188@gmail.com>
2024-07-21 12:52:41 +0400

    declear dom elements

--- sript.js
+++ sript.js
@@ -1,1 +1,12 @@
-let randomNumber = Math.floor(Math.random() * 6) + 1;
+const btnNew = document.querySelector(".btn-new");
+const btnHold = document.querySelector(".btn-hold");
+const btnRoll = document.querySelector(".btn-roll");
+const finalScore = document.querySelector(".final-score");
+const dice = document.querySelector(".dice");
+
+btnRoll.addEventListener("click", () => {
+  let randomNumber = Math.floor(Math.random() * 6) + 1;
+
+  dice.src = "img/dice-" + randomNumber + ".png";
+  dice.style.display = "block";
+});
